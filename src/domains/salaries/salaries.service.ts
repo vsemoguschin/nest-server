d33b386d32e17203fa9e657c07717e04f9b4594f
Alexey <sushak@mail.ru>
2025-03-24 12:58:00 +0300

    avito-salaries

--- src/domains/salaries/salaries.service.ts
+++ src/domains/salaries/salaries.service.ts
@@ -16,22 +16,24 @@ export class SalariesService {
       },
       include: {
         users: {
-          where: {
-            role: {
-              shortName: 'MOP',
-            },
-          },
+          where: { role: { shortName: 'MOP' } },
           include: {
             role: true,
             workSpace: true,
             dealSales: {
               where: {
+                deal: { period },
+              },
+              include: {
                 deal: {
-                  period,
+                  include: {
+                    client: true,
+                  },
                 },
               },
             },
-            dops: {
+            dops: { where: { period } },
+            managerReports: {
               where: {
                 period,
               },
@@ -41,17 +43,148 @@ export class SalariesService {
       },
     });
 
-    return workSpace?.users.map((u) => {
+    if (!workSpace?.users.length) {
+      return {
+        users: [],
+        topTotalSales: [],
+        topDopSales: [],
+        topDimmerSales: [],
+        topSalesWithoutDesigners: [],
+        topConversionDayToDay: [],
+      };
+    }
+
+    const userData = workSpace.users.map((u) => {
       const dealSales = u.dealSales.reduce((a, b) => a + b.price, 0);
       const dopSales = u.dops.reduce((a, b) => a + b.price, 0);
+      const totalSales = dealSales + dopSales;
+      const userDeals = u.dealSales.flatMap((d) => d.deal);
+      const totalCalls = u.managerReports.reduce((a, b) => a + b.calls, 0);
+
+      const conversion = totalCalls
+        ? +((userDeals.length / totalCalls) * 100).toFixed(2)
+        : 0;
+
+      const averageBill = userDeals.length
+        ? +(totalSales / userDeals.length).toFixed()
+        : 0;
+
+      const dopBonus = +(dopSales * 0.1).toFixed();
+
+      let salesBonus = 0;
+      let bonus = 0;
+
+      if (totalSales < 400_000) {
+        salesBonus = +(totalSales * 0.03).toFixed();
+      } else if (totalSales < 560_000) {
+        salesBonus = +(totalSales * 0.035).toFixed();
+      } else if (totalSales < 680_000) {
+        salesBonus = +(totalSales * 0.04).toFixed();
+      } else if (totalSales < 800_000) {
+        salesBonus = +(totalSales * 0.045).toFixed();
+        bonus = 10480;
+      } else if (totalSales < 1_000_000) {
+        salesBonus = +(totalSales * 0.05).toFixed();
+        bonus = 15000;
+      } else if (totalSales < 1_100_000) {
+        salesBonus = +(totalSales * 0.05).toFixed();
+        bonus = 17500;
+      } else if (totalSales < 1_200_000) {
+        salesBonus = +(totalSales * 0.05).toFixed();
+        bonus = 20000;
+      } else if (totalSales < 1_350_000) {
+        salesBonus = +(totalSales * 0.05).toFixed();
+        bonus = 23700;
+      } else if (totalSales < 1_500_000) {
+        salesBonus = +(totalSales * 0.05).toFixed();
+        bonus = 27500;
+      } else if (totalSales < 1_700_000) {
+        salesBonus = +(totalSales * 0.05).toFixed();
+        bonus = 32500;
+      } else if (totalSales < 2_000_000) {
+        salesBonus = +(totalSales * 0.05).toFixed();
+        bonus = 40000;
+      }
+
       return {
         id: u.id,
-        manager: u.fullName,
-        totalSalary: 0,
-        dealSales,
-        dopSales,
+        manager: u.fullName, //менеджер
+        totalSalary: salesBonus + bonus, //ЗП(₽)
+        salesBonus, //% с продаж(₽)
+        dopBonus, //% с допов(₽)
+        bonus, //премия(₽)
+        totalSales, //продажи(₽)
+        dealSales, //сделки(₽)
+        dopSales, //допы(₽)
+        conversion, //конверсия(%)
+        averageBill, //средний чек(₽)
+        topBonus: 0,
       };
     });
+
+    // - Самая высокая Сумма Заказов в отделе
+    const topDealSales = [...userData]
+      .sort((a, b) => b.dealSales - a.dealSales)
+      .slice(0, 1)
+      .map((u) => ({
+        id: u.id,
+        user: u.manager,
+        sales: u.dealSales,
+        category: 'Топ суммы заказов',
+      }));
+    // - Самая высокая сумма Допов в отделе
+    const topDopSales = [...userData]
+      .sort((a, b) => b.dopSales - a.dopSales)
+      .slice(0, 1)
+      .map((u) => ({
+        id: u.id,
+        user: u.manager,
+        sales: u.dopSales,
+        category: 'Топ суммы допов',
+      }));
+    // - Самый Высокий средний чек в отделе
+    const topAverageBill = [...userData]
+      .sort((a, b) => b.averageBill - a.averageBill)
+      .slice(0, 1)
+      .map((u) => ({
+        id: u.id,
+        user: u.manager,
+        sales: u.averageBill,
+        category: 'Топ средний чек',
+      }));
+    // - Самая высокая конверсия в отделе
+    const topConversion = [...userData]
+      .sort((a, b) => b.conversion - a.conversion)
+      .slice(0, 1)
+      .map((u) => ({
+        id: u.id,
+        user: u.manager,
+        sales: u.conversion,
+        category: 'Топ конверсия',
+      }));
+
+    userData.map((u) => {
+      if (
+        u.id === topDealSales[0].id ||
+        u.id === topDopSales[0].id ||
+        u.id === topAverageBill[0].id ||
+        u.id === topConversion[0].id
+      ) {
+        u.topBonus += 2000;
+        u.totalSalary += 2000;
+      }
+      return u;
+    });
+
+    return {
+      userData,
+      topList: [
+        topDealSales[0],
+        topDopSales[0],
+        topAverageBill[0],
+        topConversion[0],
+      ],
+    };
   }
 
   async getVKMopSalaries(user: UserDto, period: string) {
@@ -91,7 +224,14 @@ export class SalariesService {
     });
 
     if (!workSpace?.users.length) {
-      return { users: [], topTotalSales: [], topDopSales: [] };
+      return {
+        users: [],
+        topTotalSales: [],
+        topDopSales: [],
+        topDimmerSales: [],
+        topSalesWithoutDesigners: [],
+        topConversionDayToDay: [],
+      };
     }
 
     // Расчет продаж для каждого пользователя
