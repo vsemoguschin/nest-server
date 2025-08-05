cbdb0784c3005300bff89467f09aed31e99df707
Alexey <sushak@mail.ru>
2025-08-05 19:42:25 +0300

    upd

--- src/seeds/seed-update.ts
+++ src/seeds/seed-update.ts
@@ -5,40 +5,14 @@ const prisma = new PrismaClient();
 
 async function main() {
   try {
-    const names = ['ИзиСветильник', 'ИзиБук', 'Авито'];
-
-    await prisma.group.update({
+    const names = [20, 21, 23, 22];
+    prisma.group.deleteMany({
       where: {
-        id: 3,
-      },
-      data: {
-        title: 'ВК',
-      },
-    });
-
-    await prisma.group.create({
-      data: {
-        title: 'ИзиКурс',
-        workSpaceId: 2,
+        id: {
+          in: names,
+        },
       },
     });
-
-    Promise.all(
-      names.map(async (n) => {
-        // await prisma.workSpace.create({
-        //   data: {
-        //     title: n,
-        //     department: 'COMMERCIAL',
-        //   },
-        // });
-        await prisma.group.create({
-          data: {
-            title: n,
-            workSpaceId: 3,
-          },
-        });
-      }),
-    );
   } catch (e) {
     console.log(e);
   }
