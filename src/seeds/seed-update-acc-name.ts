3122a33f1b95a0e38f86254587f335437fb81d32
Alexey <sushak@mail.ru>
2025-10-23 19:51:33 +0300

    u

--- src/seeds/seed-update-acc-name.ts
+++ src/seeds/seed-update-acc-name.ts
@@ -3,35 +3,22 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  // const acc = await prisma.planFactAccount.update({
-  //   where: {
-  //     id: 2
-  //   },
-  //   data: {
-  //     name: 'Счет 4658'
-  //   }
-  // })
-  // await prisma.planFactAccount.create({
-  //   data: {
-  //     name: 'Счет ИзиБук',
-  //     accountNumber: '40802810900002610999'
-  //   }
-  // })
-  // console.log(acc);
-  const cards = await prisma.kanbanTask.findMany({
+  const acc = await prisma.planFactAccount.update({
     where: {
-      archived: true,
-      columnId: {
-        in: [23, 24],
-      },
+      id: 2
     },
-  });
-  console.log(
-    cards.map((c) => {
-      const { title, chatLink } = c;
-      return { title, chatLink };
-    }),
-  );
+    data: {
+      name: 'Счет 4658'
+    }
+  })
+  await prisma.planFactAccount.create({
+    data: {
+      name: 'Счет ИзиБук',
+      accountNumber: '40802810900002610999'
+    }
+  })
+  // console.log(acc);
+
 }
 
 main()
