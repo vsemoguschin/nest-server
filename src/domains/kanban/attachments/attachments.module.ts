fe74c4cb671c0b8b492bcfc780aa72bedd9c6b26
Alexey <sushak@mail.ru>
2025-12-08 15:23:16 +0300

    upd

--- src/domains/kanban/attachments/attachments.module.ts
+++ src/domains/kanban/attachments/attachments.module.ts
@@ -3,9 +3,17 @@ import { AttachmentsController } from './attachments.controller';
 import { AttachmentsService } from './attachments.service';
 import { TaskFilesService } from 'src/services/boards/task-files.service';
 import { TaskAuditService } from 'src/services/boards/task-audit.service';
+import { TelegramService } from 'src/services/telegram.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AttachmentsController],
-  providers: [AttachmentsService, TaskFilesService, TaskAuditService],
+  providers: [
+    AttachmentsService,
+    TaskFilesService,
+    TaskAuditService,
+    TelegramService,
+  ],
 })
 export class AttachmentsModule {}
--- src/domains/kanban/comments/comments.module.ts
+++ src/domains/kanban/comments/comments.module.ts
@@ -3,9 +3,17 @@ import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { TaskFilesService } from 'src/services/boards/task-files.service';
 import { TaskAuditService } from 'src/services/boards/task-audit.service';
+import { TelegramService } from 'src/services/telegram.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CommentsController],
-  providers: [CommentsService, TaskFilesService, TaskAuditService]
+  providers: [
+    CommentsService,
+    TaskFilesService,
+    TaskAuditService,
+    TelegramService,
+  ],
 })
 export class CommentsModule {}
