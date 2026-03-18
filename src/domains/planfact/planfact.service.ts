aee99e5ed71f224864b5ca2cd4900f1811dc9da2
Alexey <sushak@mail.ru>
2026-03-18 23:12:37 +0300

    upd

--- src/domains/planfact/planfact.service.ts
+++ src/domains/planfact/planfact.service.ts
@@ -2216,8 +2216,8 @@ export class PlanfactService {
           'https://business.tbank.ru/openapi/api/v1/statement',
           {
             proxy: false,
-            httpAgent: tbankProxyAgent,
-            httpsAgent: tbankProxyAgent,
+            // httpAgent: tbankProxyAgent,
+            // httpsAgent: tbankProxyAgent,
             headers: {
               Authorization: 'Bearer ' + tToken,
               'Content-Type': 'application/json',
